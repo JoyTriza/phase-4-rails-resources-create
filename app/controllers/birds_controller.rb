3802a66c431dcd93d4e44c635d4a42cfe7769b3c
Trizah <wangarijoytriza@gmail.com>
2022-07-19 22:58:52 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -7,13 +7,23 @@ class BirdsController < ApplicationController
   end
 
   # GET /birds/:id
+  # def show
+  #   bird = Bird.find_by(id: params[:id])
+  #   if bird
+  #     render json: bird
+  #   else
+  #     render json: { error: "Bird not found" }, status: :not_found
+  #   end
+  # end
+
   def show
     bird = Bird.find_by(id: params[:id])
-    if bird
-      render json: bird
-    else
-      render json: { error: "Bird not found" }, status: :not_found
-    end
+    render json: bird
+  end
+
+  def create
+    bird =Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
   end
 
 end
